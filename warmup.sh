2232cdb0a206f9a822bed52f23234e4d0b2db5e5
whiteday_dreamer@163.com <whiteday_dreamer@163.com>
2016-07-10 11:02:44 +0800

    realize control functions: if,for,while,until,case

--- warmup.sh
+++ warmup.sh
@@ -3,11 +3,111 @@
 
 #!/bin/bash
 
+echo ---------- welcome back to shell!! ---------------
+
+# blank can not be exist in value assignment
+rundate=`date`
+runname=`who`
+
+teststr0=""
+teststr1="aabbccdd"
+teststr2="bbccddee"
+teststr3="China beijing,China shanxi,China hangzhou,China shanghai"
+
+testnum1=10
+testnum2=30
+testnum3=50
+testnum4=100
 
 
-echo welcome back!!
+echo "date: $rundate"
+echo "who : $runname"
 
 
+# condition is a cmd
+if ls -la
+then
+	echo "cmd ls -la exe OK"
+elif sdfg
+then
+	echo "sdfg is exe failed"
+fi
+
+# condition is num compare
+if [ $testnum1 -ne $testnum2 ]
+then
+	echo "$testnum1 != $testnum2"
+elif [ $testnum1 -eq 10 ]
+then
+	echo "$testnum1 == 10"
+elif [ $testnum1 -lt $testnum3 ]
+then
+	echo "$testnum1 <= $testnum3"
+else 
+	echo "testnum1 is $testnum1"
+fi
 
 
+# condition is string compare
+if [ $teststr1 != $teststr2 ]
+then
+	echo "$teststr1 != $teststr2"
+fi
+
+if [ $teststr1 \> $teststr2 ]
+then
+	echo "$teststr1 > $teststr2"
+else
+	echo "$teststr1 < $teststr2"
+if
+
+if [ -z "$teststr0" ]
+then
+	echo "empty str"
+fi
+
+if [ -n "$teststr1" ]
+then
+	echo "no empty, $teststr1"
+fi
+
+# change IFS
+IFS_OLD=$IFS
+IFS=$','
+for a in $teststr3
+do
+echo "$a"
+done
+IFS=$IFS_OLD
+
+for ((a=0; a<10; a++))
+do
+echo "next num is $a"
+done
+
+
+while [ $testnum1 -ne 20 ]
+do
+	echo "this loop, num is $testnum1"
+	$testnum1 = $[ $testnum1 + 1 ]
+done
+
+until [ $testnum1 -ge 10 ]
+do
+	echo "this loop, num is $testnum1"
+	$testnum1 = $[ $testnum1 - 1 ]
+done
+
+
+gamename="lily"
+case $gamename in
+lily)
+	echo "vip, please enjoy you visit!";;
+dora)
+	echo "hello, dora";;	
+tucan)
+	echo "hello, tucan";;	
+*)
+	echo "hello, guest";;
+esac
 
